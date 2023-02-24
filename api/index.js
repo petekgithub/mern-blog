05fa1c7c0c1887edaef434e1ebe4a8f041da9abf
Petech <petekshamitbeyli@gmail.com>
2023-02-25 02:48:13 +0300

    mongoose-connection

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://blog:LZazom05mX4D5j1e@cluster0.e9tva6e.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.post("/register", (req, res) => {
-  res.json("test ok");
+  const { username, password } = req.body;
+
+  res.json({ requestData: { username, password } });
 });
 
 app.listen(4000);
+
+// username: blog password: LZazom05mX4D5j1e
+// mongodb+srv://blog:LZazom05mX4D5j1e@cluster0.e9tva6e.mongodb.net/?retryWrites=true&w=majority
