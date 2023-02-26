1f5784a4843b408a2f45edb3841ef09afc421750
Petech <petekshamitbeyli@gmail.com>
2023-02-26 15:52:00 +0300

    registration-control

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
@@ -10,10 +11,17 @@ mongoose.connect(
   "mongodb+srv://blog:LZazom05mX4D5j1e@cluster0.e9tva6e.mongodb.net/?retryWrites=true&w=majority"
 );
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password,
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 app.listen(4000);
